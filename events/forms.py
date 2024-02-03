e49f23fbf6eb1b45a7b559571f429533c262ebf0
shafiqlipse <ssekittoshafiq@gmail.com>
2024-02-03 16:53:58 +0300

    date

--- events/forms.py
+++ events/forms.py
@@ -6,3 +6,8 @@ class CapacityForm(forms.ModelForm):
     class Meta:
         model = capacityBuilding
         fields = "__all__"
+        widgets = {
+            "date_of_birth": forms.DateInput(
+                attrs={"type": "date", "class": "datepicker"}
+            ),
+        }
