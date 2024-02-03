63ed6a5cf03fca21f044682aa20c56f97704b923
shafiqlipse <ssekittoshafiq@gmail.com>
2024-02-03 20:20:18 +0300

    media

--- core/urls.py
+++ core/urls.py
@@ -2,10 +2,11 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.urls import path
 from events.views import *
-
+from .views import *
 
 
 urlpatterns = [
-
-
+    path("accreditation/", accreditation, name="accreditation"),
+    path("verify/", verify, name="verify"),
+    path("accreditaion_form/", accreditaionform, name="accform"),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- core/views.py
+++ core/views.py
@@ -5,3 +5,13 @@ from django.shortcuts import render
 def home(request):
     context = {}
     return render(request, "main/home.html", context)
+
+
+def accreditation(request):
+    return render(request, "media/acc.html")
+
+def verify(request):
+    return render(request, "media/verify.html")
+
+def accreditaionform(request):
+    return render(request, "media/acc_fom.html")
